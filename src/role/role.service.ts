3b40b9ea59d2596eeb4f01877539549812c25042
Wojciech Wąsek <wasekw@gmail.com>
2023-11-09 11:31:11 +0100

    create db connection

--- src/role/role.service.ts
+++ src/role/role.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { CreateRoleDto } from './dto/role.dto';
+import { RoleEntity } from './dto/role.entity';
+
+/**
+ * Role mamagment.
+ */
 
 @Injectable()
-export class RoleService {}
+export class RoleService {
+
+    /**
+     * Create staff member.
+     */
+
+    async addRole(role: CreateRoleDto): Promise<string> {
+        try {
+            const newRole = role as RoleEntity
+            const response = await newRole.save()
+            return (response).id
+        } catch (error) {
+            throw error
+        }
+    }
+}
