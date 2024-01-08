2535b74487dc23b58d0c8625d18fac9f5e223603
Wojciech Wąsek <wasekw@gmail.com>
2024-01-08 19:50:00 +0100

    updete role Dto

--- src/modules/role/dto/role.dto.ts
+++ src/modules/role/dto/role.dto.ts
@@ -8,16 +8,16 @@ export class CreateRoleDto implements CreateRole {
     @IsString()
     @ApiProperty()
     readonly name: string;
+
+    @IsNumber()
+    @ApiProperty()
+    readonly order: number;
 }
 
 export class RoleDto extends CreateRoleDto implements Role {
     @IsString()
     @ApiProperty()
     readonly id: string;
-
-    @IsNumber()
-    @ApiProperty()
-    readonly order: number;
 }
 
 export class RolesListDto extends ListDto<RoleDto> {
